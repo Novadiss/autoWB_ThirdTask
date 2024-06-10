c93f81ecabfd4f46dfb989355a59ba7757cc3116
Novadis <dark.novadis@gmail.com>
2024-06-10 16:41:57 +0300

    apdate second task

--- playwright/tests/Invalid.test.js
+++ playwright/tests/Invalid.test.js
@@ -1,22 +1,20 @@
 const { test, expect } = require("@playwright/test");
 test("test", async ({ page }) => {
-  // Go to https://netology.ru/
   await page.goto("https://netology.ru/");
-  // Click text=Войти
+  await page.waitForTimeout(3000);
+  await page.screenshot({ path: "./Screenshots/invalidTest/screenshot.png" });
   await Promise.all([
     page.waitForNavigation(/*{ url: 'https://netology.ru/?modal=sign_in' }*/),
     page.click("text=Войти"),
   ]);
-  // Click [placeholder="Email"]
+  await page.waitForTimeout(3000);
+  await page.screenshot({ path: "./Screenshots/invalidTest/screenshot1.png" });
   await page.click('[placeholder="Email"]');
-  // Fill [placeholder="Email"]
   await page.fill('[placeholder="Email"]', "asdasasd@asdasd.ru");
-  // Click [placeholder="Пароль"]
   await page.click('[placeholder="Пароль"]');
-  // Fill [placeholder="Пароль"]
   await page.fill('[placeholder="Пароль"]', "asddsa213");
-  // Click [data-testid="login-submit-btn"]
   await page.click('[data-testid="login-submit-btn"]');
-  // Click [data-testid="login-error-hint"]
   await page.click('[data-testid="login-error-hint"]');
+  await page.waitForTimeout(3000);
+  await page.screenshot({path: "./Screenshots/invalidTest/screenshot2.png"});
 });
--- playwright/tests/Valid.test.js
+++ playwright/tests/Valid.test.js
@@ -1,12 +1,16 @@
-const { test, expect } = require('@playwright/test');
-test('test', async ({ page }) => {
-  // Go to https://netology.ru/
-  await page.goto('https://netology.ru/');
-  // Click text=Войти
+const { test, expect } = require("@playwright/test");
+const { validLogin, validPassword } = require("../user");
+
+test("test", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.waitForTimeout(3000);
+  await page.screenshot({ path: "./Screenshots/validTest/screenshot.png" });
   await Promise.all([
     page.waitForNavigation(/*{ url: 'https://netology.ru/?modal=sign_in' }*/),
-    page.click('text=Войти')
+    page.click("text=Войти"),
   ]);
+  await page.waitForTimeout(3000);
+  await page.screenshot({ path: "./Screenshots/validTest/screenshot1.png" });
   // Click [placeholder="Email"]
   await page.click('[placeholder="Email"]');
   // Fill [placeholder="Email"]
@@ -18,10 +22,11 @@ test('test', async ({ page }) => {
   // Click [data-testid="login-submit-btn"]
   await Promise.all([
     page.waitForNavigation(/*{ url: 'https://netology.ru/profile/8754719' }*/),
-    page.click('[data-testid="login-submit-btn"]')
+    page.click('[data-testid="login-submit-btn"]'),
   ]);
+  await page.waitForTimeout(4000);
+  await page.screenshot({ path: "./Screenshots/validTest/screenshot2.png" });
   // Click text=Моё обучение
-  await page.click('text=Моё обучение');
-  await expect(page).toHaveURL('https://netology.ru/profile/8754719');
-  // Click [data-testid="profile-programs-content"] >> text=Моё обучение
+  await page.click("text=Моё обучение");
+  await expect(page).toHaveURL("https://netology.ru/profile/8754719");
 });
